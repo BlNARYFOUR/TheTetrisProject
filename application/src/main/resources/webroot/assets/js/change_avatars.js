c5e827ae6d693efad54a7b91ec361a8c9686985a
Helsens Bryan <bryan.helsens@student.howest.be>
2018-11-07 14:47:19 +0100

    Confirm scherm toegevoegd

--- application/src/main/resources/webroot/assets/js/change_avatars.js
+++ application/src/main/resources/webroot/assets/js/change_avatars.js
@@ -3,8 +3,12 @@
 document.addEventListener("DOMContentLoaded", showAllImagesOfASpecificPage);
 
 let elementSelectables;
+let selectedItem = null;
 
 function showAllImagesOfASpecificPage() {
+    document.getElementById("yes").disabled = true;
+    document.getElementById("no").disabled = true;
+
     document.getElementById("back").addEventListener("click", goBack);
     document.getElementById("save").addEventListener("click", saveAvatar);
 
@@ -17,6 +21,7 @@ function showAllImagesOfASpecificPage() {
 
         firstSelected = "";
     }
+
     location.innerHTML = imgList;
     selectablesEvents();
 }
@@ -47,12 +52,43 @@ function changeSelected(e) {
 
 }
 
-
-
 function goBack() {
     location.href="main_menu.html";
 }
 
 function saveAvatar() {
+    console.log("disabled back and save");
+    document.getElementById("back").disabled = true;
+    document.getElementById("save").disabled = true;
+    document.getElementById("yes").disabled = false;
+    document.getElementById("no").disabled = false;
+
+    document.getElementById("confirm").classList.remove("hidden");
+    document.getElementById("confirm").classList.add("show");
+
+    document.getElementById("yes").addEventListener("click", confirmYes);
+    document.getElementById("no").addEventListener("click", confirmNo);
+}
+
+function confirmYes(e) {
+    e.preventDefault();
+    console.log("testid");
+
+    location.href="main_menu.html";
+    //TODO verander de avatar naar de gekozen avatar
+
+}
+
+function confirmNo() {
+    document.getElementById("back").disabled = false;
+    document.getElementById("save").disabled = false;
+    document.getElementById("yes").disabled = true;
+    document.getElementById("no").disabled = true;
+
+    document.getElementById("confirm").classList.remove("show");
+    document.getElementById("confirm").classList.add("hidden");
+
+    //TODO de eerste avatar selecteren
+    //TODO selectedItem terug op null
 
 }
