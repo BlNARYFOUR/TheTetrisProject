d173213f1975784f853d7e8c3ef16ff292c3db61
Helsens Bryan <bryan.helsens@student.howest.be>
2018-11-08 14:42:16 +0100

    dailystreak in main_menu gestoken

--- application/src/main/resources/webroot/assets/js/dailystreaks.js
+++ application/src/main/resources/webroot/assets/js/dailystreaks.js
@@ -8,48 +8,91 @@ let photo = "../assets/media/retroBlocks.png";
 let days = 7;
 let severalDaysLoggedIn = 3;
 
+let alreadyLoggedIn = true;
+
 function showDailyRewards(e) {
     e.preventDefault();
-    console.log("dailyRewards");
 
-    document.getElementById("close").addEventListener("click", closeDailyStreaks);
+    if (alreadyLoggedIn === false){
+        disabledButtons();
 
-    let location = document.getElementById("dailyRewards");
-    let imgList = "";
-    // todo bijhouden van elke hij al heeft ontvangen => Backend
+        document.getElementById("close").addEventListener("click", closeDailyStreaks);
 
-    for (let i = 1; i < (days + 1); i++){
+        let location = document.getElementById("dailyRewards");
+        let imgList = "";
+        // todo bijhouden van elke hij al heeft ontvangen => Backend
 
-        if (rewards[i] === "cubes"){
-            photo = "../assets/media/cube_logo.png";
+        for (let i = 1; i < (days + 1); i++){
+
+            if (rewards[i] === "cubes"){
+                photo = "../assets/media/cube_logo.png";
+            }
+
+            if (severalDaysLoggedIn >= i){
+                imgList += "<figure id=day_'" + i + "'>" +
+                    "<h2>Day " + i + "</h2>" +
+                    "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
+                    "<i class='material-icons' id='doneMark'>done</i>" +
+                    "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
+                    "</figure>";
+            } else {
+                imgList += "<figure id=day_'" + i + "'>" +
+                    "<h2>Day " + i + "</h2>" +
+                    "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
+                    "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
+                    "</figure>";
+            }
+            // dit kan later worden verwijdert (de regel hieronder) !!!!!!
+            photo = "../assets/media/retroBlocks.png";
+            //TODO src linken met naam van rewards
         }
 
-        if (severalDaysLoggedIn >= i){
-            imgList += "<figure id=day_'" + i + "'>" +
-                "<h2>Day " + i + "</h2>" +
-                "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
-                "<i class='material-icons' id='doneMark'>done</i>" +
-                "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
-                "</figure>";
-        } else {
-            imgList += "<figure id=day_'" + i + "'>" +
-                "<h2>Day " + i + "</h2>" +
-                "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
-                "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
-                "</figure>";
-        }
-        // dit kan later worden verwijdert (de regel hieronder) !!!!!!
-        photo = "../assets/media/retroBlocks.png";
-        //TODO src linken met naam van rewards
+        location.innerHTML = imgList;
+
+        alreadyLoggedIn = true;
+    }else {
+        closeDailyStreaks(e);
     }
 
-    location.innerHTML = imgList;
 }
+
+function disabledButtons() {
+    document.getElementById("logOut").disabled = true;
+    document.getElementById("chooseGamemode").disabled = true;
+    document.getElementById("clan").disabled = true;
+    document.getElementById("shop").disabled = true;
+    document.getElementById("highScore").disabled = true;
+    document.getElementById("buyCubes").disabled = true;
+    document.getElementById("close").disabled = false;
+}
+
 
 function closeDailyStreaks(e) {
     e.preventDefault();
 
-    document.getElementById("dailystreaks").classList.remove("showDailyRewards");
-    document.getElementById("dailystreaks").classList.add("hidden");
+    enabledButtons();
 
+    document.getElementById("dailystreaks").classList.remove("showDailyRewards");
+    document.getElementById("dailystreaks").classList.add("hiddenDailyRewards");
+
+    main_menu();
+
+}
+
+function enabledButtons() {
+    document.getElementById("logOut").disabled = false;
+    document.getElementById("chooseGamemode").disabled = false;
+    document.getElementById("clan").disabled = false;
+    document.getElementById("shop").disabled = false;
+    document.getElementById("highScore").disabled = false;
+    document.getElementById("buyCubes").disabled = false;
+    document.getElementById("close").disabled = true;
+}
+
+function main_menu() {
+    document.getElementById("chooseGamemode").addEventListener("click", chooseGamemode);
+}
+
+function chooseGamemode() {
+    location.href = "chooseGamemode.html";
 }
