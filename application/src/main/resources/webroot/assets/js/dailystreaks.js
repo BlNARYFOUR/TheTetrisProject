3dac5a5cd27b1209fc797ec7ab9a672be98e72dc
Helsens Bryan <bryan.helsens@student.howest.be>
2018-11-08 13:20:51 +0100

    Checkmarker toegevoegd voor het aantal dagen dat je na elkaar hebt ingelogd

--- application/src/main/resources/webroot/assets/js/dailystreaks.js
+++ application/src/main/resources/webroot/assets/js/dailystreaks.js
@@ -4,27 +4,52 @@ document.addEventListener("DOMContentLoaded", showDailyRewards);
 
 let rewards = ["", "xp", "scratch card", "xp", "scratch card", "xp", "mystery box", "cubes"];
 let amount = ["", 50, 1, 100, 1, 150, 1, 10];
+let photo = "../assets/media/retroBlocks.png";
 let days = 7;
+let severalDaysLoggedIn = 3;
 
-function showDailyRewards() {
+function showDailyRewards(e) {
+    e.preventDefault();
     console.log("dailyRewards");
+
+    document.getElementById("close").addEventListener("click", closeDailyStreaks);
 
     let location = document.getElementById("dailyRewards");
     let imgList = "";
     // todo bijhouden van elke hij al heeft ontvangen => Backend
 
     for (let i = 1; i < (days + 1); i++){
-        imgList += "<figure id=day_'" + i + "'>" +
-            "<h2>Day " + i + "</h2>" +
-            "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='../assets/media/retroBlocks.png' class='rewards'>" +
-            "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
-            "</figure>"
+
+        if (rewards[i] === "cubes"){
+            photo = "../assets/media/cube_logo.png";
+        }
+
+        if (severalDaysLoggedIn >= i){
+            imgList += "<figure id=day_'" + i + "'>" +
+                "<h2>Day " + i + "</h2>" +
+                "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
+                "<i class='material-icons' id='doneMark'>done</i>" +
+                "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
+                "</figure>";
+        } else {
+            imgList += "<figure id=day_'" + i + "'>" +
+                "<h2>Day " + i + "</h2>" +
+                "<img data-dailysteak=day_'" + i + "_" + rewards[i] + "' src='" + photo + "' class='rewards'>" +
+                "<figcaption>+ " + amount[i] + " " + rewards[i] + "</figcaption>" +
+                "</figure>";
+        }
+        // dit kan later worden verwijdert (de regel hieronder) !!!!!!
+        photo = "../assets/media/retroBlocks.png";
         //TODO src linken met naam van rewards
     }
 
     location.innerHTML = imgList;
+}
 
+function closeDailyStreaks(e) {
+    e.preventDefault();
 
-
+    document.getElementById("dailystreaks").classList.remove("showDailyRewards");
+    document.getElementById("dailystreaks").classList.add("hidden");
 
 }
