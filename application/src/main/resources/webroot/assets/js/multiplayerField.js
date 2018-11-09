2102a3ec048b7e83febba147fd62a324ba951ea9
Van Acker Reinbert <reinbert.van.acker@student.howest.be>
2018-11-09 11:16:38 +0100

    multiplayerField-ready for singleplayer

--- application/src/main/resources/webroot/assets/js/multiplayerField.js
+++ application/src/main/resources/webroot/assets/js/multiplayerField.js
@@ -10,7 +10,6 @@ const GAME_BOARD_WIDTH = 10;
 const GAME_BOARD_HEIGHT = 18;
 
 const SPACE_TOP = 10 + BLOCK;
-const SPACE_BETWEEN = 86;
 const HOLD_WIDTH = 4;
 const HOLD_HEIGHT = 4;
 
@@ -19,14 +18,16 @@ const IMAGES = {
     restartPopupPane: createImageObj("restartPopupPane.png"),
     button01: createImageObj("button01.png"),
     restartButton: createImageObj("restartButton.png"),
-    backgroundTile: createImageObj("backgroundTile.png"),
+    retroBackgroundTile: createImageObj("retroBackgroundTile.png"),
     darkBlueTile: createImageObj("darkBlueTile.png"),
     lightBlueTile: createImageObj("lightBlueTile.png"),
     orangeTile: createImageObj("orangeTile.png"),
     redTile: createImageObj("redTile.png"),
     yellowTile: createImageObj("yellowTile.png"),
     greenTile: createImageObj("greenTile.png"),
-    purpleTile: createImageObj("purpleTile.png")
+    purpleTile: createImageObj("purpleTile.png"),
+    pikachuTile: createImageObj("pikachu.png")
+
 };
 
 const COLORS = {
@@ -37,7 +38,8 @@ const COLORS = {
     YELLOW: 4,
     LIGHT_BLUE: 5,
     GREEN: 6,
-    PURPLE: 7
+    PURPLE: 7,
+    PIKA: 8
 };
 
 let c;
@@ -54,7 +56,7 @@ function init(e) {
     c.height = 18 * BLOCK;
 
     tiles = new Map();
-    tiles.set(COLORS.TRANSPARENT, IMAGES.backgroundTile);
+    tiles.set(COLORS.TRANSPARENT, IMAGES.retroBackgroundTile);
     tiles.set(COLORS.RED, IMAGES.redTile);
     tiles.set(COLORS.DARK_BLUE, IMAGES.darkBlueTile);
     tiles.set(COLORS.ORANGE, IMAGES.orangeTile);
@@ -62,18 +64,26 @@ function init(e) {
     tiles.set(COLORS.YELLOW, IMAGES.yellowTile);
     tiles.set(COLORS.GREEN, IMAGES.greenTile);
     tiles.set(COLORS.PURPLE, IMAGES.purpleTile);
+    tiles.set(COLORS.PIKA, IMAGES.pikachuTile);
 
     createGameBoard();
     createHold();
-    createText();
+    createText("Next", SPACE + 1.25 * BLOCK + GAME_BOARD_SPACING + GAME_BOARD_WIDTH * BLOCK, SPACE_TOP - 5);
+    createText("Hold", SPACE + 1.2 * BLOCK, SPACE_TOP - 5);
+    createText("Level", SPACE + 1.1 * BLOCK, SPACE_TOP + 12 * BLOCK);
+    createText("Score", SPACE + 1.25 * BLOCK + GAME_BOARD_SPACING + GAME_BOARD_WIDTH * BLOCK, SPACE_TOP + 12 * BLOCK);
+    createText("3898", SPACE + 1.3 * BLOCK + GAME_BOARD_SPACING + GAME_BOARD_WIDTH * BLOCK, SPACE_TOP + 14.2 * BLOCK);
+    createValue("5", BLOCK * 2.0, SPACE_TOP + 14.5 * BLOCK);
     drawUserField();
 }
 
 function drawUserField() {
-
     drawGameBoard();
     drawHold();
-    
+    drawNext();
+    drawHero();
+    drawCircle(BLOCK * 2.5, SPACE_TOP + 14 * BLOCK,50,0,2*Math.PI);
+    drawCircle(SPACE + 2.0 * BLOCK + GAME_BOARD_SPACING + GAME_BOARD_WIDTH * BLOCK, SPACE_TOP + 14 * BLOCK,50,0,2*Math.PI)
 }
 
 function createGameBoard() {
@@ -114,15 +124,42 @@ function drawHold() {
     }
 }
 
-function createText() {
+function drawNext() {
+    for (let i = 0; i < HOLD_HEIGHT; i ++) {
+        for (let j = 0; j < HOLD_WIDTH; j ++) {
+            ctx.drawImage(tiles.get(hold[i][j]), j * BLOCK + SPACE + GAME_BOARD_SPACING + GAME_BOARD_WIDTH * BLOCK, i * BLOCK + SPACE_TOP, BLOCK, BLOCK)
+        }
+    }
+}
+
+function createText(text, x_co, y_co) {
     ctx.font = 0.8 * BLOCK + "px fabian, sans-serif";
     ctx.fillStyle = "#9584FF";
-    ctx.fillText("Hold", SPACE + BLOCK, SPACE_TOP - 5);
+    ctx.fillText(text, x_co, y_co);
+}
+
+function createValue(number, x_co, y_co) {
+    ctx.font = 2.0 * BLOCK + "px fabian, sans-serif";
+    ctx.fillStyle = "#9584FF";
+    ctx.fillText(number, x_co, y_co);
 }
 
 function createImageObj(fileName) {
     let img = new Image();
     img.src = "../assets/media/" + fileName;
     return img;
+}
+
+function drawHero() {
+    ctx.drawImage(IMAGES.pikachuTile, 1.2 * BLOCK, SPACE_TOP + 6.2 * BLOCK, BLOCK * 3, BLOCK * 3);
+}
+
+function drawCircle(x, y, radius, startAngle, endAngle, counterClockWise) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle, counterClockWise);
+    ctx.fillStyle = "#9584FF";
+    ctx.globalAlpha = 0.35;
+    ctx.fill();
+    ctx.stroke();
 }
 
