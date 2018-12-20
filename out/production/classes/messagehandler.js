2dd5598e6dfdbbb584e60eef6ca59f6bb899e301
Brend Lambert <45264166+BlNARYFOUR@users.noreply.github.com>
2018-12-20 12:21:58 +0100

    Test

--- out/production/classes/messagehandler.js
+++
@@ -1,9 +0,0 @@
-var eb = vertx.eventBus();
-
-eb.consumer("login.events.message", function(message) {
-    console.log(message.body());
-    message.reply("OK");
-    eb.publish("login.events.main", message.body());
-});
-
-eb.consumer("")
