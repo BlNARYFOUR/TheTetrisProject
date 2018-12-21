95093d7047cc38fec7a7ef38957495efc83d7c86
Reinbert <reinbertva@gmail.com>
2018-12-21 11:24:50 +0100

    added sound

--- src/main/resources/webroot/assets/js/gameSounds.js
+++ src/main/resources/webroot/assets/js/gameSounds.js
@@ -4,32 +4,65 @@ document.addEventListener("DOMContentLoaded", init);
 
 const SOUNDS = {
     lineScored: createAudioObj("lineScored.mp3"),
-    rotate: createAudioObj("rotate.mp3")
+    rotate: createAudioObj("rotate.mp3"),
+    hardHit: createAudioObj("hardHit.mp3")
 };
-
-/*const KEY_DOWN_FUNC = {
-    "KeyW": tryAndRotate,
-    "ArrowUp": tryAndRotate,
-};*/
 
 function init() {
     window.addEventListener("keydown", keyIn);
 
-}
+    // lineScored();
 
-function tryAndRotate() {
-    SOUNDS.rotate.volume = 0.3;
-    SOUNDS.rotate.play();
 }
 
 function keyIn(e) {
     console.log("testUp");
     console.log(e.code);
     let key = e.code;
-    if (key === "ArrowUp" || key === "KeyW") {
-        tryAndRotate();
+
+    switch (key) {
+        case "ArrowUp":
+        case "KeyW":
+            SOUNDS.rotate.volume = 0.3;
+            SOUNDS.rotate.play();
+            break;
+        case "Space":
+            SOUNDS.hardHit.play();
+            break;
     }
 }
+
+/*function lineScored() {
+    let scored = checkAndScoreFullRow();
+
+    if (scored) {
+        SOUNDS.lineScored.play();
+    }
+}
+
+function checkAndScoreFullRow() {
+    let isFullLine = true;
+    let totalLineScore = 0;
+    let hasScored = false;
+
+    for (let i = 0; i < GAME_BOARD_HEIGHT; i++) {
+        totalLineScore = 0;
+        isFullLine = true;
+        for (let j = 0; j < GAME_BOARD_WIDTH; j++) {
+            if (0 < GAME_BOARDS[i][j]) {
+                totalLineScore += GAME_BOARDS[i][j];
+            } else {
+                isFullLine = false;
+            }
+        }
+
+            if (isFullLine) {
+                console.log("You scored a line!");
+                hasScored = true;
+            }
+        }
+        return hasScored;
+}*/
 
 function createAudioObj(fileName) {
     let audio = document.createElement("audio");
